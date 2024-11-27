fa38fe075ce092958e51c5e4e5541d7118e49a6b
Mateusz Walas <matwal777@gmail.com>
2024-11-27 01:59:27 +0100

    remove start and end time fields from Reservation model

--- lib/models/reservation.dart
+++ lib/models/reservation.dart
@@ -3,16 +3,12 @@ class Reservation {
   final String parkingPrettyId;
   final String status;
   final String date;
-  final String startTime;
-  final String endTime;
 
   Reservation({
     required this.parkingSpotId,
     required this.parkingPrettyId,
     required this.status,
     required this.date,
-    required this.startTime,
-    required this.endTime,
   });
 
   factory Reservation.fromJson(Map<String, dynamic> json) {
@@ -21,8 +17,6 @@ class Reservation {
       parkingPrettyId: json['pretty_id'] as String,
       status: json['status'] as String,
       date: json['reservation_date'] as String,
-      startTime: json['start_time'] as String,
-      endTime: json['end_time'] as String,
     );
   }
 }
