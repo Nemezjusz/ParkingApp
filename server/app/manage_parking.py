4c049bb3bd8b4770df2519b6fca0332acc64c709
Nemezjusz <szymon.sadowski.m@gmail.com>
2024-12-01 21:39:37 +0100

    resolved error with blink red and blue

--- server/app/manage_parking.py
+++ server/app/manage_parking.py
@@ -7,7 +7,7 @@ from bson import ObjectId  # type: ignore
 import string
 
 
-MONGO_URL = "mongodb://mongodb:27017"
+MONGO_URL = "mongodb://localhost:27017"
 client = AsyncIOMotorClient(MONGO_URL)
 db = client.parking_system
 parking_spots_col = db.parking_spots
